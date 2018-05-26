1e7b1c9458517958eb9f6731f7d645dede113a32
CHristelle <CHristelle@DESKTOP-MV2QIEM>
2018-05-26 12:24:12 -0400

    fin capitulo 12

---
+++ EjerciciosCsharp/Capitulo12/ejercicios3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EjerciciosCsharp.Capitulo12
+{
+    class ejercicios3
+    {
+        public void cap12_3()
+        {
+            Console.WriteLine("Excepciones de arreglos:\n1-IndexOutOfRangeException.\n3-ArgumentException.\n4-ArgumentOutOfRangeException.5-ArrayTypeMismatchException");
+        }
+    }
+}
